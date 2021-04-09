e39abb2c7d0dffa22761b699fbea15a96eff67ca
grenader <igor.kanshyn@gmail.com>
2021-04-08 20:43:35 -0400

    added ability to deploy to Google App Engine

--- src/main/java/com/grenader/stocksdash/controllers/StocksController.java
+++ src/main/java/com/grenader/stocksdash/controllers/StocksController.java
@@ -5,12 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StocksController {
 
     @Autowired
     private StockLineViewService stockLineViewService;
+
+    @GetMapping
+    public ModelAndView method() {
+        return new ModelAndView("redirect:" + "/stocks");
+    }
 
     @GetMapping("/stocks")
     public String listProducts(Model model){
